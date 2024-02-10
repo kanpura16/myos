5a36a463515194ae43dd8f25cf35757f0901a8d2
kanpura <kanpura16@gmail.com>
2024-02-10 19:18:32 +0900

    refactor

--- src/pci.zig
+++ src/pci.zig
@@ -22,7 +22,7 @@ pub fn scanAllBuses() void {
     } else {
         var function: u8 = 0;
         while (function < 8) : (function += 1) {
-            if (readVendorID(0, 0, function) == 0xffff) continue;
+            if (readVendorId(0, 0, function) == 0xffff) continue;
             scanBus(function);
         }
     }
@@ -37,13 +37,13 @@ fn scanBus(bus: u8) void {
 
 fn scanDevice(bus: u8, device: u8) void {
     var function: u8 = 0;
-    if (readVendorID(bus, device, function) == 0xffff) return;
+    if (readVendorId(bus, device, function) == 0xffff) return;
     addDevice(.{ .bus = bus, .device = device, .function = function, .class_code = readClassCode(bus, device, function) });
     scanFunction(bus, device, function);
 
     if (!isSingleFuncDev(readHeaderType(bus, device, function))) {
         while (function < 8) : (function += 1) {
-            if (readVendorID(bus, device, function) == 0xffff) continue;
+            if (readVendorId(bus, device, function) == 0xffff) continue;
             addDevice(.{ .bus = bus, .device = device, .function = function, .class_code = readClassCode(bus, device, function) });
             scanFunction(bus, device, function);
         }
@@ -67,16 +67,16 @@ fn addDevice(device: Device) void {
     num_device += 1;
 }
 
-pub fn readVendorID(bus: u8, device: u8, function: u8) u16 {
-    return @intCast(readIOAddrSpace(makeIOPortAddr(bus, device, function, 0)) & 0xffff);
+pub fn readVendorId(bus: u8, device: u8, function: u8) u16 {
+    return @intCast(readIOAddrSpace(makeIoPortAddr(bus, device, function, 0)) & 0xffff);
 }
 
 fn readHeaderType(bus: u8, device: u8, function: u8) u8 {
-    return @intCast(readIOAddrSpace(makeIOPortAddr(bus, device, function, 0x0c)) >> 16 & 0xff);
+    return @intCast(readIOAddrSpace(makeIoPortAddr(bus, device, function, 0x0c)) >> 16 & 0xff);
 }
 
 fn readClassCode(bus: u8, device: u8, function: u8) ClassCode {
-    const class_code: u32 = readIOAddrSpace(makeIOPortAddr(bus, device, function, 8));
+    const class_code: u32 = readIOAddrSpace(makeIoPortAddr(bus, device, function, 8));
     return .{
         .base = @intCast(class_code >> 24 & 0xff),
         .sub = @intCast(class_code >> 16 & 0xff),
@@ -85,14 +85,14 @@ fn readClassCode(bus: u8, device: u8, function: u8) ClassCode {
 }
 
 fn readSecondaryBus(bus: u8, device: u8, function: u8) u8 {
-    return @intCast(readIOAddrSpace(makeIOPortAddr(bus, device, function, 0x18)) >> 8 & 0xff);
+    return @intCast(readIOAddrSpace(makeIoPortAddr(bus, device, function, 0x18)) >> 8 & 0xff);
 }
 
 fn isSingleFuncDev(header_type: u8) bool {
     return (header_type & 0b1000_0000) == 0;
 }
 
-fn makeIOPortAddr(bus: u8, device: u8, function: u8, reg_offset: u8) u32 {
+fn makeIoPortAddr(bus: u8, device: u8, function: u8, reg_offset: u8) u32 {
     return 1 << 31 | @as(u32, @intCast(bus)) << 16 | @as(u32, @intCast(device)) << 11 | @as(u32, @intCast(function)) << 8 | (reg_offset & 0b1111_1100);
 }
 
@@ -100,28 +100,28 @@ const config_addr_reg_addr: u16 = 0xcf8;
 const config_data_reg_addr: u16 = 0xcfc;
 
 fn writeIOAddrSpace(addr: u32, data: u32) void {
-    IOOut32(config_addr_reg_addr, addr);
-    IOOut32(config_data_reg_addr, data);
+    ioOut32(config_addr_reg_addr, addr);
+    ioOut32(config_data_reg_addr, data);
 }
 
 fn readIOAddrSpace(addr: u32) u32 {
-    IOOut32(config_addr_reg_addr, addr);
-    return IOIn32(config_data_reg_addr);
+    ioOut32(config_addr_reg_addr, addr);
+    return ioIn32(config_data_reg_addr);
 }
 
-extern fn IOOut32(addr: u16, data: u32) void;
-extern fn IOIn32(addr: u16) u32;
+extern fn ioOut32(addr: u16, data: u32) void;
+extern fn ioIn32(addr: u16) u32;
 
 comptime {
     asm (
-        \\IOOut32:
+        \\ioOut32:
         // %dx = addr;
         \\  mov %di, %dx
         // %eax = data;
         \\  mov %esi, %eax
         \\  out %eax, %dx
         \\
-        \\IOIn32:
+        \\ioIn32:
         // %dx = addr
         \\  mov %di, %dx
         \\  in %dx, %eax
